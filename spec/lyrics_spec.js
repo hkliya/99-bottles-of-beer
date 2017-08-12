b6083d025b7fbbbf5d4044351a5a8838fb2afd74
Seaborn Lee <seabornlee.cn@gmail.com>
2017-08-12 08:46:22 +0800

    Generate lyrics given 2

--- spec/lyrics_spec.js
+++ spec/lyrics_spec.js
@@ -24,4 +24,14 @@ No more bottles of beer on the wall, no more bottles of beer.
 Go to the store and buy some more, 99 bottles of beer on the wall.`);
     });
 
+    it("should generate lyrics given 2", function () {
+        let lyrics = toLyrics(2);
+        expect(lyrics).to.equal(`2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.
+1 bottle of beer on the wall, 1 bottle of beer.
+Take one down and pass it around, no more bottles of beer on the wall.
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.`);
+    });
+
 });
