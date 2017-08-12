88b4f0c1e1866e27e33b61fd229ea7e073c76f1a
Seaborn Lee <seabornlee.cn@gmail.com>
2017-08-12 08:36:53 +0800

    Generate lyrics given 1

--- spec/lyrics_spec.js
+++ spec/lyrics_spec.js
@@ -10,9 +10,18 @@ var toLyrics = require("../lib/lyrics.js");
 
 
 describe("Lyrics", function() {
-    it("should generate lyrics", function () {
+    it("should generate lyrics given 0", function () {
         let lyrics = toLyrics(0);
         expect(lyrics).to.equal(`No more bottles of beer on the wall, no more bottles of beer.
 Go to the store and buy some more, 99 bottles of beer on the wall.`);
     });
+
+    it("should generate lyrics given 1", function () {
+        let lyrics = toLyrics(1);
+        expect(lyrics).to.equal(`1 bottle of beer on the wall, 1 bottle of beer.
+Take one down and pass it around, no more bottles of beer on the wall.
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.`);
+    });
+
 });
